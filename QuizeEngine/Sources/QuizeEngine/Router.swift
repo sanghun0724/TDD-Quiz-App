f1d61dc8224f9cc611f7924e40d3f22396a46dbe
sanghun0724 <sanghun0724@naver.com>
2024-03-03 17:48:26 +0900

    feat start change router protocol without breaking client  (S2-1)

--- QuizeEngine/Sources/QuizeEngine/Router.swift
+++ QuizeEngine/Sources/QuizeEngine/Router.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
+// naming flowDelegate 탈락 -> flow는 게임엔진의 internal 구현임. (🌟 leak implementation)
+public protocol QuizDelegate {
+  associatedtype Question: Hashable
+  associatedtype Answer
+  
+  func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
+  func handle(result: Result<Question, Answer>)
+}
+
+@available(*, deprecated)
 public protocol Router {
   associatedtype Question: Hashable
   associatedtype Answer
@@ -14,3 +24,10 @@ public protocol Router {
   func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
   func routeTo(result: Result<Question, Answer>)
 }
+
+// change router protocol without brake
+// Steps (commit)
+// add deprecated message router protocol
+// create new protocol
+// remove Hashable constraint from Question and make the result type generic
+//
